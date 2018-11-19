d708dad47b13366de73e8de97104a7687e386426
AlexanderDmitriev <alexanderdmv@gmail.com>
2018-11-19 21:24:50 +0300

    added subscribe

--- lib/deribit/ws.rb
+++ lib/deribit/ws.rb
@@ -6,14 +6,14 @@ module Deribit
     URL = ENV['WS_DOMAIN'] || 'wss://www.deribit.com/ws/api/v1/'
     AVAILABLE_EVENTS = [:order_book, :trade, :user_order]
 
-    attr_reader :socket, :response, :ids_stack
+    attr_reader :socket, :response, :ids_stack, :handler
 
     def initialize(api_key, api_secret, handler = Handler)
       @credentials = Credentials.new(api_key, api_secret)
       @request     = Request.new(@credentials)
       @socket      = connect
 
-      @handler    = handler
+      @handler    = handler.new
       @ids_stack  = []
       start_handle
     end
@@ -22,18 +22,30 @@ module Deribit
       WebSocket::Client::Simple.connect(URL)
     end
 
+    def reconnect!
+      @socket = connect
+      start_handle
+    end
+
     def ping
       message = {action: "/api/v1/public/ping"}
       @socket.send(message.to_json)
     end
 
-    def subscribe(instruments: [] , events: [])
-      raise "Events must include only #{AVAILABLE_EVENTS.join(", ")} actions" if events.map{|e| AVAILABLE_EVENTS.include?(e)}.index(false) or events.empty?
+    # events to be reported, possible events:
+    # "order_book" -- order book change
+    # "trade" -- trade notification
+    # "announcements" -- announcements (list of new announcements titles is send)
+    # "user_order" -- change of user orders (openning, cancelling, filling)
+    # "my_trade" -- filtered trade notification, only trades of the
+    # subscribed user are reported with trade direction "buy"/"sell" from the
+    # subscribed user point of view ("I sell ...", "I buy ..."), see below.
+    # Note, for "index" - events are ignored and can be []
+    def subscribe(instruments=[] , events: ["user_order"])
+      raise "Events must include only #{AVAILABLE_EVENTS.join(", ")} actions" if events.map{|e| AVAILABLE_EVENTS.include?(e.to_sym)}.index(false) or events.empty?
       raise "instruments are required" if instruments.empty?
-
-      params[:events]    = events
-      params[:arguments] = arguments
-      send(path: '/api/v1/private/subscribe', arguments: params)
+      arguments = {instrument: instruments, event: events}
+      send(path: '/api/v1/private/subscribe', arguments: arguments)
     end
 
     def account
@@ -44,17 +56,38 @@ module Deribit
       send(path: '/api/v1/public/getinstruments', arguments: {expired: expired})
     end
 
+    def getcurrencies
+      send(path: '/api/v1/public/getcurrencies')
+    end
+
+    def handle_notifications(notifications)
+      return if notifications.empty?
+      notification, *tail = notifications;
+      handler.send(notification["message"], notification["result"])
+      handle_notifications(tail)
+    end
+
     private
 
     def start_handle
+      instance = self
       @socket.on :message do |msg|
-        p msg
         if msg.type == :text
-
           json = JSON.parse(msg.data)
-          puts json
+          #if find query send json to handler
+          if json["id"] and stack_id = instance.ids_stack.find{|i| i[json["id"]]}
+            method  = stack_id[json["id"]]
+            #pass the method to handler
+            instance.ids_stack.delete(stack_id)
+            instance.handler.send(method, json)
+          elsif json["notifications"]
+            instance.handle_notifications(json["notifications"])
+          else
+            raise "A handle method not found for #{json}!"
+          end
+
         elsif msg.type == :close
-          reconnect!
+          instance.reconnect!
         end
       end
 
@@ -68,25 +101,17 @@ module Deribit
       end
     end
 
-    def reconnect!
-      @socket = connect
-      start_handle
-    end
 
     def send(path: , arguments: {})
       return unless path
-      #reconnect if need_reconnect?
-
       params = {action: path, arguments: arguments}
       sig = @request.generate_signature(path, arguments)
-      p sig
       params[:sig] = sig
-      params[:id], id = Time.now.to_i
+      params[:id] = Time.now.to_i
 
       action = path[/\/api.*\/([^\/]+)$/, 1]
-      put_id(id, action)
+      put_id(params[:id], action)
 
-      p params
       @socket.send(params.to_json)
     end
 
